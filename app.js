0351bca545dd4529369f7b668b8a6535db8d83a1
ouss2231 <o_bouhen@live.concordia.ca>
2024-09-28 12:39:25 -0400

    add a "search notification " in the notification buttom

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const db = new pg.Client({
   user: "postgres",
   host: "localhost",
   database: "Oussama341",
-  password: process.env.DB_PASSWORD, 
+  password: "HondaCivic2025!", 
   port: 5432, 
 });
 db.connect();
