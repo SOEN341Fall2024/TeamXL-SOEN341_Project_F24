e32ee9d8c3b173b2422600c4f0888623ee6a6a5a
Joyal Biju K. <122915447+Joyal99@users.noreply.github.com>
2024-09-30 21:25:50 -0400

    Changes made

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ import bodyParser from "body-parser";
 import pg from "pg";
 import bcrypt from "bcrypt";
 import dotenv from "dotenv";
-import session from express-session;
+import session from "express-session";
 
 dotenv.config();
 
@@ -17,7 +17,7 @@ const saltRounds = 10;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
-app.use(session({ secret: "key" }));
+app.use(session({ secret: "key", resave: false, saveUninitialized: true }));
 
 // Create a new PostgreSQL client for database connection
 const db = new pg.Client({
