549566dfbdee65086be5227b0ea1e15fcc14254b
aditiingle <aditiingle14@gmail.com>
2024-10-26 10:50:32 -0400

    Fixed Form style for create teams

--- student_teacher_relationship.sql
+++ student_teacher_relationship.sql
@@ -5,8 +5,7 @@ CREATE SEQUENCE student_id_seq START 1000;  -- Start student IDs from 1000
 CREATE TABLE INSTRUCTOR (
     ID SERIAL PRIMARY KEY, --Autoincrement to put unique ID
     NAME VARCHAR(100), 
-    PASSWORD VARCHAR(1000) NOT NULL,
-    course_name varchar(50) UNIQUE
+    PASSWORD VARCHAR(1000) NOT NULL
 );
 
 -- Create table for groups
@@ -19,35 +18,25 @@ CREATE TABLE GROUPS (
 CREATE TABLE STUDENT (
     ID INTEGER PRIMARY KEY DEFAULT nextval('instructor_id_seq'), --Autoincrement to put unique ID
     NAME VARCHAR(100),
-    ID_GROUP INTEGER,
-    ID_teacher INTEGER,
     PASSWORD VARCHAR(1000) NOT NULL,
     FOREIGN KEY (ID_GROUP) REFERENCES GROUPS(ID_GROUP),
     FOREIGN KEY (ID_teacher) REFERENCES INSTRUCTOR(ID)
 );
+
 -- Create table for the evaluation
 CREATE TABLE EVALUATION (
-    ID_EVALUATION SERIAL PRIMARY KEY,
     ID_EVALUATOR INTEGER,
     ID_EVALUATEE INTEGER,
-    ID_GROUP INTEGER,
-    FOREIGN KEY (ID_GROUP) REFERENCES GROUPS(ID_GROUP),  
+    cooperation INT,
+    conceptual_contribution INT,  
+    practical_contribution INT,
+    work_ethic INT,
+    comments TEXT,
     FOREIGN KEY (ID_EVALUATOR) REFERENCES STUDENT(ID),
     FOREIGN KEY (ID_EVALUATEE) REFERENCES STUDENT(ID),
     UNIQUE (ID_EVALUATOR, ID_EVALUATEE)
 );
 
-
-/*
--- Create join table for many-to-many relationship between STUDENT and GROUPS
-CREATE TABLE STUDENT_GROUPS (
-    ID_STUDENT INTEGER,
-    ID_GROUP INTEGER,
-    PRIMARY KEY (ID_STUDENT, ID_GROUP), -- Composite primary key to avoid duplicates
-    FOREIGN KEY (ID_STUDENT) REFERENCES STUDENT(ID),
-    FOREIGN KEY (ID_GROUP) REFERENCES GROUPS(ID_GROUP)
-);
-*/
 
 
 -- Create an index to order by STUDENT_NAME
